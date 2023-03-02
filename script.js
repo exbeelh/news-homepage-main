ea441abe48c5abf9562144805f5185560c68dfb9
Ardian Maulana <exbeelh@gmail.com>
2023-03-02 13:17:24 +0700

    first commit

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const menuButton = document.querySelector(".button_menu");
-const closeButton = document.querySelector(".closeButton");
+const closeButton = document.querySelector(".button_close");
+const navbar = document.querySelector(".main_nav");
+
+menuButton.addEventListener("click", () => {
+  navbar.classList.add("active");
+});
+
+closeButton.addEventListener("click", () => {
+  navbar.classList.remove("active")
+})
